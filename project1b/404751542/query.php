c4535ee1b2f3bd92a28c02e09ab9d3859e7dc2b9
Jennie Zheng <jennie@jenniezheng.com>
2018-04-13 10:51:57 -0700

    added php and queries

---
+++ project1b/404751542/query.php
@@ -0,0 +1,49 @@
+<html>
+  <p>
+    Type a query!
+    <br/>
+    Example: Select * FROM Actor WHERE id=11;
+    
+    <form method="GET">
+      <input type="text" name="expr"><br>
+      <input type="submit" value="Calculate">
+    </form>
+  </p>
+  <?php
+    $servername="localhost";
+    $username="cs143";
+    $password="";
+    $conn=new mysqli($servername,$username,$password,'TEST');
+    if($conn->connect_error > 0){
+       die("Connect Error");
+    }
+     echo "Connected!";
+     if (!empty($_GET['expr'])) {
+        if(FALSE)
+          echo 'Invalid Expression!';
+  else {
+  $query = $_GET['expr'];
+  $rs=$db->query($query);
+  
+
+  }
+    
+/*    set_error_handler("error_handler", E_ALL);
+    try {
+      if (!empty($_GET['expr'])) {
+        if(preg_match('/[^0-9\+\-\*\.\/]+/',$_GET['expr'],$matches, PREG_OFFSET_CAPTURE)===1)
+          echo 'Invalid Expression!';
+        else {
+          $sol= eval('return '.$_GET['expr'].';');
+          echo $_GET['expr'].'='.$sol;
+        }
+      }
+    } catch (Exception $e) {
+      echo 'Invalid Expression!';
+    }
+    function error_handler($errno, $errstr) {
+      throw new ErrorException('', $errno);
+    }
+    */
+  ?>
+</html>
