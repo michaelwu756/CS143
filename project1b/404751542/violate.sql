c9de30e8a6fdec26430912294744052135f00b66
Jennie Zheng <jennie@jenniezheng.com>
2018-04-15 11:53:49 -0700

    fixed typo in violations

--- project1b/404751542/violate.sql
+++ project1b/404751542/violate.sql
@@ -8,20 +8,20 @@ INSERT INTO MaxMovieID VALUES (-123);
 INSERT INTO Review VALUES ('Jennie', '2018-04-15 5:30:24', 2, 9000, 'WOW SO AMAZING');
 -- Rating is way too high
 
-INSERT INTO Actor VALUES(99999999, 'Jennie', 'Zheng', 'Female', 1998-03-07, NULL);
+INSERT INTO Actor VALUES(99999999, 'Jennie', 'Zheng', 'Female', '1998-03-07', NULL);
 -- id greater than MaxMovieID
 
-INSERT INTO Director VALUES(99999999, 'Michael', 'Wu', 1998-06-02, NULL);
+INSERT INTO Director VALUES(99999999, 'Michael', 'Wu', '1998-06-02', NULL);
 -- id greater than MaxMovieID
 
 -- PRIMARY KEY CONSTRAINTS
 INSERT INTO Movie VALUES(2, 'Bad Day', 2010, 'PG', 'CS143');
 -- The Movie table already has tuple with id = 2
 
-INSERT INTO Actor VALUES(10, 'Jennie', 'Zheng', 'Female', 1998-03-07, NULL);
+INSERT INTO Actor VALUES(10, 'Jennie', 'Zheng', 'Female', '1998-03-07', NULL);
 -- The Actor table already has tuple with id = 10
 
-INSERT INTO Director VALUES(16, 'Michael', 'Wu', 1998-06-02, NULL);
+INSERT INTO Director VALUES(16, 'Michael', 'Wu', '1998-06-02', NULL);
 -- The Director table already has tuple with id = 16
 
 -- FOREIGN KEY CONSTRAINTS
