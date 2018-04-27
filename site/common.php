53f08d97a4beb7eb7b2caad0398f4eae166265d6
Jennie Zheng <jenniezheng321@gmail.com>
2018-04-26 22:11:17 -0700

    adding

--- site/common.php
+++ site/common.php
@@ -1,6 +1,12 @@
 <?php
+   include("./config.php");
+?>
+<?php
+
+    $title = "CS143 DataBase Query System";
+    $email = "jennie@jenniezheng.com";
    // -----------------------------------------------------------------------
-    include("./config.php");
+
 
     function headerer(){
         print "<title>".$title."</title>\n";
@@ -8,11 +14,11 @@
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0" />
     <meta name="description" content="UCLA Coding Competition" />
     <meta name="author" content="" />
-    <title>CodeSprint LA</title>
+    <title>Movies!</title>
     <!-- Bootstrap core CSS-->
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-     <link rel="stylesheet" href="/style.css" >
+     <link rel="stylesheet" href="./style.css" >
 
     ';
     }
@@ -22,7 +28,7 @@
     print '
 
     <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-      <a class="navbar-brand" href="#"> Code <img src="/images/paw.png" style="padding-left:1px; max-height: 25px;vertical-align: text-top;">  Sprint</a>
+      <a class="navbar-brand" href="#"> CS <img src="./images/paw.png" style="padding-left:1px; max-height: 25px;vertical-align: text-top;">  143</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -31,38 +37,10 @@
         <ul class="navbar-nav mr-auto">';
 
       if ($from != "index")
-         print "<li class='nav-item'><a class='nav-link ' href='index.php'> contest </a></li>";
-      else print "<li class='nav-item'><a class='nav-link active' href='#'> contest </a></li>";
+         print "<li class='nav-item'><a class='nav-link ' href='index.php'> Index </a></li>";
+      else print "<li class='nav-item'><a class='nav-link active' href='#'> Index </a></li>";
 
-      if ($from != "scores")
-         print "<li class='nav-item'><a class='nav-link' href='scores.php'> scoreboard </a></li>";
-      else print "<li class='nav-item'><a class='nav-link active' href='#'> scoreboard </a></li>";
 
-      if ($from != "submit")
-         print "<li class='nav-item'><a class='nav-link' href='submit.php'> submissions </a></li>";
-      else print "<li class='nav-item'><a class='nav-link active' href='#'> submissions </a></li>";
-
-      if ($from != "clarify")
-         print "<li class='nav-item'><a class='nav-link' href='clarify.php'> clarifications </a></li>";
-      else print "<li class='nav-item'><a class='nav-link active' href='#'> clarifications </a></li>";
-
-      if ($from != "help")
-         print "<li class='nav-item'><a class='nav-link' href='help.php'> help </a></li>";
-      else print "<li class='nav-item'><a class='nav-link active' href='#'> help </a></li>";
-
-      print '</ul>';
-      print '<ul class="navbar-nav navbar-right">';
-      // toggle between displaying "Log In" and "Log Out"
-      if (empty($_SESSION["teamid"]))
-      {
-         if ($from != "login")
-            print "<a class='nav-link' href='login.php'>log in</a>";
-         else print "<a class='nav-link active' href='#'>log in</a>";
-      }
-      else
-      {
-         print "<a class='nav-link' href='login.php'>log out</a>";
-      }
 
       print '</ul>
       </div>
@@ -74,28 +52,14 @@
    {
     print '<hr/>';
       print '<footer class="container py-5">
-      <div class="row">
+      <div class="row ">
         <div class="col-12 col-md">
-          <img src="/images/bear.png" style="margin-bottom:5px"></img>
-          <p class="d-block mb-3 text-muted">UCLA CodeSprint 2018<br/>
-            Hosted by ACM - ICPC
-    </div>
+          <img src="./images/bear.png" style="margin-bottom:5px"></img>
+          <p class="d-block mb-3 text-muted">CS143 Movies System 2018<br/>
+            Created by Jennie Zheng and Michael Wu
 
-    <div class="col-12 col-md">
-          <h5>Credits</h5>
-          <p class="d-block mb-3 text-muted">
-          Ultra Cool Programming<br/>
-           Contest Control Centre v1.8<br/>
-        Copyright &copy; 2005-2010<br/>
-         By Sonny Chan</p>
-    </div>
-
-        <div class="col-12 col-md">
-          <h5>Questions?</h5>
-          <p class="text-muted">For your questions, comments, and issues...<br/>
-            please contact your host today at<br>
-            </p>
-            <a class="text-muted" href="mailto:'.$email.'">'.$email.'</a>
+         </p>
+     </div>
       </div>
     </footer>
 
--- site/index.php
+++ site/index.php
@@ -1,7 +1,4 @@
 <?php
-// This line is edited remotedly!
-?>
-<?php
    include("./config.php");
    include("./common.php");
 ?>
@@ -17,25 +14,9 @@
       <?php navigation("index"); ?>
 
 <div class="container text-center" style='padding-top:5em;'>
-<h1>Contest</h1>
-
+<h1>Main</h1>
 
 <?php
-   // just make sure the submitfile and judgefile are there
-   if (!file_exists($g_submitfile))
-   {
-      $fp = fopen($g_submitfile, "w");
-      fclose($fp);
-      chmod($g_submitfile, 0660);
-   }
-   if (!file_exists($g_judgefile))
-   {
-      $fp = fopen($g_judgefile, "w");
-      fclose($fp);
-      chmod($g_judgefile, 0660);
-   }
-
-?><?php
 
 ?>
 
