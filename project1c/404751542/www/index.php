e9404653564e4f95f9bfef77e7718b367f8e138e
Michael Wu <cheeserules43@gmail.com>
2018-04-29 20:26:38 -0700

    linted add_actor.php, index.php

--- project1c/404751542/www/index.php
+++ project1c/404751542/www/index.php
@@ -1,43 +1,31 @@
 <?php
-   include("./config.php");
-   include("./common.php");
+  include("./config.php");
+  include("./common.php");
 ?>
-
 <html>
-
-<head>
-     <?php headerer(); ?>
-</head>
-
-
-<body >
-      <?php navigation(); ?>
-
-<div>
-
-
-  <div class="jumbotron jumbotron-fluid ">
-          <div class="container">
-            <h1 class="display-4">Welcome to Rotten Oranges!</h1>
-        <p style='margin-top:30px;'>Feel free to add to our database or search for interesting movies!</p>
+  <head>
+    <?php headerer(); ?>
+  </head>
+  <body>
+    <?php navigation(); ?>
+    <div>
+      <div class="jumbotron jumbotron-fluid ">
+        <div class="container">
+          <h1 class="display-4">Welcome to Rotten Oranges!</h1>
+          <p style='margin-top:30px;'>Feel free to add to our database or search for interesting movies!</p>
+        </div>
       </div>
-    </div>';
-
-<div class="container ">
-  <div class="row ">
-    <div class="col-12 col-md">
-      <img src="./images/bear.png" style="margin-bottom:5px"></img>
-      <p class="d-block mb-3 text-muted">CS143 Movie Database 2018<br/>
-        Created by Jennie Zheng and Michael Wu
-     </p>
- </div>
-</div>
-
-
-</div>
-
-<?php footer(); ?>
-
-</body>
-
+      <div class="container ">
+        <div class="row ">
+          <div class="col-12 col-md">
+            <img src="./images/bear.png" style="margin-bottom:5px"></img>
+            <p class="d-block mb-3 text-muted">
+              CS143 Movie Database 2018<br/>
+              Created by Jennie Zheng and Michael Wu
+            </p>
+          </div>
+        </div>
+      </div>
+    <?php footer(); ?>
+  </body>
 </html>
