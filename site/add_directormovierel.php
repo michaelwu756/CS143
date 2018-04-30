77d6bd305b2c2a3d7acd6cd75ec971fc535b5863
Jennie Zheng <jenniezheng321@gmail.com>
2018-04-29 19:39:18 -0700

    adding

--- site/add_directormovierel.php
+++ site/add_directormovierel.php
@@ -31,36 +31,34 @@
 ?>
 
 <?php
+    $directors = get_list_directors();
+    $directorOptions='';
+    foreach($directors as $director){
+        $newDirector= sprintf('<option value="%u">%s %s (%s)</option>',$director['id'],$director['first'],$director['last'],$director['dob']);
+        $directorOptions=$directorOptions.$newDirector;
+    }
 
-$directors = get_list_directors();
-$directorOptions='';
-foreach($directors as $director){
-    $newDirector= sprintf('<option value="%u">%s %s (%s)</option>',$director['id'],$director['first'],$director['last'],$director['dob']);
-    $directorOptions=$directorOptions.$newDirector;
-}
-
-$movies = get_list_movies();
-$moviesOptions='';
-foreach($movies as $movie){
-    $newMovie= sprintf('<option value="%u">%s (%u)</option>',$movie['id'],$movie['title'],$movie['year']);
-    $moviesOptions=$moviesOptions.$newMovie;
-}
-
+    $movies = get_list_movies();
+    $moviesOptions='';
+    foreach($movies as $movie){
+        $newMovie= sprintf('<option value="%u">%s (%u)</option>',$movie['id'],$movie['title'],$movie['year']);
+        $moviesOptions=$moviesOptions.$newMovie;
+    }
 
 
-form('<form method="POST" action="add_directormovierel.php">
-    <div class="form-group">
-        <label for="movie">Movie</label>
-        <select class="form-control" name="movie" required>'.$moviesOptions.'
-        </select>
-    </div>
-    <div class="form-group">
-        <label for="director">Director</label>
-        <select class="form-control" name="director" required>'.$directorOptions.'
-        </select>
-    </div>
-        <button type="submit" name="submit" class="btn btn-default">Add It!</button>
-    </form>'); ?>
+    form('<form method="POST" action="add_directormovierel.php">
+        <div class="form-group">
+            <label for="movie">Movie</label>
+            <select class="form-control" name="movie" required>'.$moviesOptions.'
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="director">Director</label>
+            <select class="form-control" name="director" required>'.$directorOptions.'
+            </select>
+        </div>
+            <button type="submit" name="submit" class="btn btn-default">Add It!</button>
+        </form>'); ?>
 
 <?php footer(); ?>
 
