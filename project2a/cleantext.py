5e36f2a6c934a23a5fd3a90ca92fd0dda75c4a9f
Austin Guo <austinguo550@gmail.com>
2018-05-17 16:47:30 -0700

    fixed bad punctuation matcher, may fail for some edge cases though

--- project2a/cleantext.py
+++ project2a/cleantext.py
@@ -3,15 +3,18 @@ import re
 squeeze_space = re.compile(r'[\t\n ]+')
 url_matcher = re.compile(r'https?:\/\/[^)]*')
 punctuation_matcher = re.compile(r'([?.,:;!])')
-bad_punctuation_matcher = re.compile(r'[^A-Za-z?!.,;:]\s+|\s+[^A-Za-z?!.,;:]')      # is fucked up but is semi-working (doesn't work for the url case)
+bad_punctuation_matcher = re.compile(r'[^A-Za-z0-9?!.,;: ][^A-Za-z0-9?!.,;:]|[^A-Za-z0-9?!.,;:][^A-Za-z0-9?!.,;: ]')      # is fucked up but is semi-working (doesn't work for the url case)
 
 def sanitize(s):
-    print s
+    print "original string: " + s
     res = squeeze_space.sub(' ', s)     # 1 squeeze spaces into 1 space (also covers # 3)
     res = url_matcher.sub('', res)      # 2 remove urls
     res = punctuation_matcher.sub(r' \1 ', res) # 4 separate external pucntuation (putting spaces between punctuation we want)
     res = bad_punctuation_matcher.sub(' ', res)  # 5 remove bad punctuation that isn't inside a word
     res = res.lower()                     # 6
+
+
+    res = squeeze_space.sub(' ', res)
     return res
 
 
