433c22b696629cf518c1555f5cfc2d5a43215c0a
Austin Guo <austinguo550@gmail.com>
2018-05-17 17:07:20 -0700

    Added unigrams bigrams trigrams

--- project2a/cleantext.py
+++ project2a/cleantext.py
@@ -6,24 +6,55 @@ punctuation_matcher = re.compile(r'([?.,:;!])')
 bad_punctuation_matcher = re.compile(r'[^A-Za-z0-9?!.,;: ][^A-Za-z0-9?!.,;:]|[^A-Za-z0-9?!.,;:][^A-Za-z0-9?!.,;: ]')      # is fucked up but is semi-working (doesn't work for the url case)
 
 def sanitize(s):
-    print "original string: " + s
+    #### Parsing
+
+    # print "original string: " + s
     res = squeeze_space.sub(' ', s)     # 1 squeeze spaces into 1 space (also covers # 3)
     res = url_matcher.sub('', res)      # 2 remove urls
     res = punctuation_matcher.sub(r' \1 ', res) # 4 separate external pucntuation (putting spaces between punctuation we want)
     res = bad_punctuation_matcher.sub(' ', res)  # 5 remove bad punctuation that isn't inside a word
     res = res.lower()                     # 6
 
-
     res = squeeze_space.sub(' ', res)
-    return res
 
+    tokens = res.split(' ')
+    n = len(tokens)
+
+    #### Unigrams
+    unigram = list(filter(lambda x: x not in ['.', ',', ':', ';', '!', '?'], tokens))
+    unigram = ' '.join(unigram)
+
+
+    #### Bigrams
+    bigrams = []
+    for i in range(0, n-1): 
+        if(punctuation_matcher.match(tokens[i]) is None and punctuation_matcher.match(tokens[i+1]) is None):
+            bigrams.append(tokens[i] + "_" + tokens[i+1])
+
+    bigrams = ' '.join(bigrams)
+
+
+    #### Trigrams
+    trigrams = []
+    for i in range(0, n-2): 
+        if(punctuation_matcher.match(tokens[i]) is None and punctuation_matcher.match(tokens[i+1]) is None and punctuation_matcher.match(tokens[i+2]) is None):
+            trigrams.append(tokens[i] + "_" + tokens[i+1] + "_" + tokens[i+2])
+
+    trigrams = ' '.join(trigrams)
+
+    return (res, unigram, bigrams, trigrams)
+
+
+def format_print(tup):
+    for t in tup:
+        print(t)
 
 
 def main():
     # test 1
-    print sanitize("I'm afraid I can't explain myself, sir. Because I am not myself, you see?")
+    format_print(sanitize("I'm afraid I can't explain myself, sir. Because I am not myself, you see?"))
     # test 2
-    print sanitize("FUCK [some text](http://facebook.com) this is a url that we need'ed to remove. yaauhklh")
+    format_print(sanitize("FUCK [some text](http://facebook.com) this is a url that we need'ed to remove. yaauhklh"))
 
 
 
